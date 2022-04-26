e88c87bebf8513d75a326f092e5c6e434599f92a
ivmerk <ivan.e.merkulov@gmail.com>
2022-04-26 12:39:11 +0300

    Dragon Trainer Monthly done

--- myscript.js
+++ myscript.js
@@ -1,2 +1,30 @@
-let str = "Привет";  
-alert( [...str] ); // П,р,и,в,е,т
+// 1. Создаём новый XMLHttpRequest-объект
+let xhr = new XMLHttpRequest();
+
+// 2. Настраиваем его: GET-запрос по URL /article/.../load
+xhr.open('GET', '/article/xmlhttprequest/example/load');
+
+// 3. Отсылаем запрос
+xhr.send();
+
+// 4. Этот код сработает после того, как мы получим ответ сервера
+xhr.onload = function() {
+  if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+  } else { // если всё прошло гладко, выводим результат
+    alert(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
+  }
+};
+
+xhr.onprogress = function(event) {
+  if (event.lengthComputable) {
+    alert(`Получено ${event.loaded} из ${event.total} байт`);
+  } else {
+    alert(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
+  }
+
+};
+
+xhr.onerror = function() {
+  alert("Запрос не удался");
+};
